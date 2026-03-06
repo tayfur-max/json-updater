532844f001665b74f5f6b5a4dd530e432eb2d102
tayfur-max <tayfur@mediazonegames.com>
2026-03-06 12:14:41 +0300

    Update process_json.py

--- process_json.py
+++ process_json.py
@@ -8,9 +8,15 @@ ACCESS_KEY = os.environ["R2_ACCESS_KEY"]
 SECRET_KEY = os.environ["R2_SECRET_KEY"]
 
 BUCKET_NAME = "tables"
-RAW_KEY = "raw/bigdata.json"
 
 TOTAL_FILES = 100
+
+DATASETS = {
+    "classic": "raw/bigdata.json",
+    "super_ligue": "raw/super_ligue.json",
+    "premier_ligue": "raw/premier_ligue.json",
+    "ligue1": "raw/ligue1.json"
+}
 
 s3 = boto3.client(
     "s3",
@@ -19,30 +25,40 @@ s3 = boto3.client(
     aws_secret_access_key=SECRET_KEY,
 )
 
-# Büyük dosyayı indir
-s3.download_file(BUCKET_NAME, RAW_KEY, "bigdata.json")
-
-with open("bigdata.json", "r", encoding="utf-8") as f:
-    tables = json.load(f)
-
-TOTAL_TABLES = len(tables)
-
-if TOTAL_TABLES == 0:
-    raise Exception("JSON içinde tablo yok!")
-
-# Gün bazlı başlangıç indexi
 day_of_year = datetime.utcnow().timetuple().tm_yday
-start_index = (day_of_year * TOTAL_FILES) % TOTAL_TABLES
 
-for i in range(TOTAL_FILES):
-    index = (start_index + i) % TOTAL_TABLES
-    table = tables[index]
+for mode, key in DATASETS.items():
 
-    filename = f"table_{i+1}.json"
+    local_file = f"{mode}.json"
 
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(table, f, ensure_ascii=False)
+    # indir
+    s3.download_file(BUCKET_NAME, key, local_file)
 
-    s3.upload_file(filename, BUCKET_NAME, f"public/{filename}")
+    with open(local_file, "r", encoding="utf-8") as f:
+        tables = json.load(f)
 
-print("100 tablo başarıyla üretildi ve yüklendi.")
+    TOTAL_TABLES = len(tables)
+
+    if TOTAL_TABLES == 0:
+        raise Exception(f"{mode} JSON içinde tablo yok!")
+
+    start_index = (day_of_year * TOTAL_FILES) % TOTAL_TABLES
+
+    for i in range(TOTAL_FILES):
+        index = (start_index + i) % TOTAL_TABLES
+        table = tables[index]
+
+        filename = f"{mode}_table_{i+1}.json"
+
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(table, f, ensure_ascii=False)
+
+        s3.upload_file(
+            filename,
+            BUCKET_NAME,
+            f"public/{mode}/{filename}"
+        )
+
+    print(f"{mode} için 100 tablo üretildi.")
+
+print("Tüm modlar tamamlandı.")
